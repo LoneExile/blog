52c4d9b25b5fec4c25f26896cabdbf1c00765a3d
LoneExile <apinant-moo@tutanota.com>
2023-01-06 19:52:07 +0700

    feat: add astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,28 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import image from '@astrojs/image';
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEndpoint: '@astrojs/image/sharp'
-  }), sitemap()]
-});
+  site: 'https://blog.bunlaikun.dev',
+  integrations: [
+    tailwind(),
+    image({
+      serviceEndpoint: '@astrojs/image/sharp',
+    }),
+    sitemap(),
+    compress({
+      img: false,
+    }),
+  ],
+})
