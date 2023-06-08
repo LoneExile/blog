2e4943a6c223a88724d503742cda39392091002b
LoneExile <Apinant-Moo@tutanota.com>
2023-06-08 17:12:57 +0700

    feat: add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import {defineConfig} from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
-import node from '@astrojs/node'
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://voidbox.io',
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), preact()],
   output: 'server',
   adapter: node({
-    mode: 'standalone',
-  }),
-})
+    mode: 'standalone'
+  })
+});
