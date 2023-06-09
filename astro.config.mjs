705d2fbe5194d2f7c6d17728576362188ec190e8
LoneExile <Apinant-Moo@tutanota.com>
2023-06-09 08:15:06 +0700

    feat: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
-import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
+import node from '@astrojs/node'
+import preact from '@astrojs/preact'
 
-import preact from "@astrojs/preact";
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://voidbox.io',
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), preact()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    sitemap(),
+    preact(),
+    compress({img: false}),
+  ],
   output: 'server',
   adapter: node({
-    mode: 'standalone'
-  })
-});
+    mode: 'standalone',
+  }),
+})
