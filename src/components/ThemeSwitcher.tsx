8778820cea38ce257ad69e0208227f95999ec0c0
LoneExile <Apinant-Moo@tutanota.com>
2023-06-17 13:19:50 +0700

    feat: (HACK)lazy load toggle theme button

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,23 +3,25 @@ import {settings, SettingsValue} from '@utils/store'
 import Sun from '@icons/sun'
 import Moon from '@icons/moon'
 
-type Theme = Extract<SettingsValue['theme'], SettingsValue['theme']>
-
-const setTheme = (val: SettingsValue) => {
-  let theme: Theme = val.theme === 'dark' ? 'light' : 'dark'
-  settings.setKey('theme', theme)
-  document.body.dataset.theme = theme
-}
-
-const size = 'w-5 h-5'
-
 export default function ThemeSwitcher() {
+  type Theme = Extract<SettingsValue['theme'], SettingsValue['theme']>
+
+  const size = 'w-5 h-5'
   const $settings = useStore(settings)
+
+  const setTheme = (val: SettingsValue) => {
+    let theme: Theme = val.theme === 'dark' ? 'light' : 'dark'
+    settings.setKey('theme', theme)
+    document.body.dataset.theme = theme
+  }
+
   return (
-    <label class="swap swap-rotate" aria-label="theme switcher">
-      <input type="checkbox" onClick={() => setTheme($settings)} />
-      <Sun color="swap-on mt-auto fill-current" size={size} />
-      <Moon color="swap-off mt-auto fill-current" size={size} />
-    </label>
+    <>
+      <label class="swap swap-rotate" aria-label="theme switcher">
+        <input type="checkbox" onClick={() => setTheme($settings)} />
+        <Sun color="swap-on mt-auto fill-current" size={size} />
+        <Moon color="swap-off mt-auto fill-current" size={size} />
+      </label>
+    </>
   )
 }
