5700806e8b2c8f84b405b57e67be8de9f4f6bc0d
LoneExile <Apinant-Moo@tutanota.com>
2023-06-19 18:01:51 +0700

    feat: update template email, enable comment subscription

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -52,7 +52,7 @@ export function Comments() {
           components: ["embed"],
           no_footer: true,
           simple_view: true,
-          show_email_subscription: false,
+          show_email_subscription: true,
         };
         !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`
       parentElement.appendChild(script)
