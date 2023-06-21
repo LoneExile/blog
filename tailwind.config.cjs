44feb72db29ff69df61ad129a059802ce0618e73
LoneExile <Apinant-Moo@tutanota.com>
2023-06-21 11:15:09 +0700

    feat(css): updated theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,30 +5,12 @@ module.exports = {
     themes: [
       {
         dark: {
-          ...require('daisyui/src/theming/themes')['[data-theme=night]'],
-          primary: '#3abff8',
-          secondary: '#828df8',
-          accent: '#f471b5',
-          neutral: '#1d283a',
-          'base-100': '#0f1729',
-          info: '#0ca6e9',
-          success: '#2bd4bd',
-          warning: '#f4c152',
-          error: '#fb6f84',
+          ...require('daisyui/src/theming/themes')['[data-theme=business]'],
           fontFamily: 'Chalkboard',
         },
         light: {
-          ...require('daisyui/src/theming/themes')['[data-theme=lemonade]'],
-          fontFamily: {
-            sans: [
-              'Chalkboard',
-              'Inter',
-              'sans-serif',
-              'Helvetica',
-              'Arial',
-              'sans-serif',
-            ],
-          },
+          ...require('daisyui/src/theming/themes')['[data-theme=emerald]'],
+          fontFamily: 'Chalkboard',
         },
       },
     ],
