919f9109e8e4cfcc7a56d2d6e9295b24f6ad5219
Apinant.U <Apinant-Moo@tutanota.com>
2024-01-06 20:59:23 +0700

    feat(articles): InfiniteScroll at home, new ui tag, <a/> to new window

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -92,6 +92,21 @@ module.exports = {
           950: "#190107",
         },
       },
+      animation: {
+        "skew-scroll": "skew-scroll 20s linear infinite",
+      },
+      keyframes: {
+        "skew-scroll": {
+          "0%": {
+            transform:
+              "rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)",
+          },
+          "100%": {
+            transform:
+              "rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)",
+          },
+        },
+      },
     },
     fontFamily: {
       body: [
